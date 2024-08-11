4feabe61cd556379b9f06f46e319e9abf5c9fde4
Varun Mohankumar <varunmj@users.noreply.github.com>
2024-08-11 13:55:45 -0500

    new commit

--- dags/reddit_dag.py
+++ dags/reddit_dag.py
@@ -8,6 +8,7 @@ from airflow.operators.python import PythonOperator
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from pipelines.aws_s3_pipeline import upload_s3_pipeline
 from pipelines.reddit_pipeline import reddit_pipeline
 
 # dag_directory = os.path.dirname(os.path.abspath(__file__))
@@ -41,3 +42,12 @@ extract = PythonOperator(
     },
     dag=dag
 )
+#upload to s3:
+
+upload_s3 = PythonOperator(
+    task_id='s3_upload',
+    python_callable= upload_s3_pipeline,
+    dag=dag
+)
+
+extract >> upload_s3
